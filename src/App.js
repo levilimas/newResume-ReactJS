8505771544bce3e10aae8a77eb57d4adc6f7483b
levilimas <leviimediata@hotmail.com>
2021-10-14 17:23:22 -0300

    fist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function BemVindo() {
+  return <h1>Hello World!!</h1>
+}
+
+function App() {
+  return <>
+    <BemVindo />
+    <BemVindo />
+  </>;
+}
+
+export default App;
