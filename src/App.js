699a8dfc4b14e47aea9da520446ec805478f2e81
levilimas <leviimediata@hotmail.com>
2021-10-19 00:15:30 -0300

    treinando react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function BemVindo() {
-  return <h1>Hello World!!</h1>
-}
+const Site = styled.div `
+  width: 100vh;
+  height: 100vh;
+  background-color: #000;
+`;
+
+const Title = styled.h1 `
+  color: #0eb;
+  font-size: 30px;
+  text-align: center;
+`;
+
+const Botao = styled.button `
+  font-size: 19px;
+  background-color: #00FF99;
+  padding: 10px 15px;
+  border-radius: 20px;
+  color: #fff;
+  border: 2px solid #00FF00;
+  text-align: center;
+  display: flex;
+`;
 
 function App() {
-  return <>
-    <BemVindo />
-    <BemVindo />
-  </>;
+  return (
+    <Site>
+    <Title>Bem Vindo e obrigado por estar aqui Jesus.</Title>
+    <Botao>Click aqui</Botao>
+    </Site>
+  );
 }
+  
 
 export default App;
