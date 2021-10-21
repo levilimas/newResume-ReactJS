fb0a0eac05d84d0558e65fd7cef4906dbab8f84b
levilimas <leviimediata@hotmail.com>
2021-10-21 05:50:35 -0300

    ainda preparando meus components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import React from 'react';
-import styled from 'styled-components';
 
-const Site = styled.div `
-  width: 100vh;
-  height: 100vh;
-  background-color: #000;
-`;
-
-const Title = styled.h1 `
-  color: #0eb;
-  font-size: 30px;
-  text-align: center;
-`;
-
-const Botao = styled.button `
-  font-size: 19px;
-  background-color: #00FF99;
-  padding: 10px 15px;
-  border-radius: 20px;
-  color: #fff;
-  border: 2px solid #00FF00;
-  text-align: center;
-  display: flex;
-`;
 
 function App() {
   return (
-    <Site>
-    <Title>Bem Vindo e obrigado por estar aqui Jesus.</Title>
-    <Botao>Click aqui</Botao>
-    </Site>
+    <div clasName="app">
+     
+    </div>
   );
 }
   
